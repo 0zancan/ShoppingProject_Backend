cc8a1f2b01385e738e7edf99688884a406efe940
Ozan Can <48280981+0zancan@users.noreply.github.com>
2021-09-01 22:44:27 +0300

    added generic constraint

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    public interface IEntityRepository<T> where T : class
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
